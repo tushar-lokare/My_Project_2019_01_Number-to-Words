72d13476fdd61b2b36e1f516018a55cef37d0f8b
Tushar Lokare <contacttusharlokare@gmail.com>
2019-05-05 18:25:21 +0530

    Create Pojo for only one input.
    Only one input, hence only one field is needed & it's getter setter

---
+++ Pojo.java
@@ -0,0 +1,20 @@
+package com.pojo;
+
+public class Pojo {
+
+	private String input;
+
+	public String getInput() {
+		return input;
+	}
+
+	public void setInput(String input) {
+		this.input = input;
+	}
+
+	public Pojo(String input) {
+		super();
+		this.input = input;
+	}
+
+}
